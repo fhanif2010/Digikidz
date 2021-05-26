e035a25c74ef89319f1a2c179369327e8967643e
nur faizin <nurfaizin318@gmail.com>
2021-05-26 16:15:34 +0700

    add cuti form

--- Page/Cuti/Cuti2/index.js
+++ Page/Cuti/Cuti2/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     View,
     Text,
@@ -7,31 +7,137 @@ import {
     Image,
     KeyboardAvoidingView,
     ScrollView,
+    Dimensions,
+    Modal,
+    TouchableOpacity
+
 } from "react-native";
+import moment from "moment";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {
-    faArrowAltCircleLeft,
-    faPlus,
-} from "@fortawesome/free-solid-svg-icons";
+const Cuti2 = () => {
 
-class Cuti2 extends React.Component {
+
+    const width = Dimensions.get("screen").width;
+    const height = Dimensions.get("screen").height;
+
+
+    const [cutiData, setCutiData] = useState({ name: "", position: "", start: new Date(), end: new Date(), neccessity: "",rangeDate:"" })
+    const [modalDate, setModaldate] = useState({ start: false, end: false });
+
+    const [newDate, setNewDate] = useState(new Date())
+
+
+
+    const countRangeDate = () => {
+
+        const oneDay = 24 * 60 * 60 * 1000;
+
+
+        const startYear = cutiData.start.getUTCFullYear();
+        const startMonth = cutiData.start.getMonth() + 1;
+        const startDay = cutiData.start.getDate() + 1;
+
+
+        const endYear = cutiData.end.getUTCFullYear();
+        const endMonth = cutiData.end.getMonth() + 1;
+        const endDay = cutiData.end.getDate() + 1;
+
+        const startDate = new Date(startYear, startMonth, startDay)
+        const endDate = new Date(endYear, endMonth, endDay)
+        const diffDays = Math.round(Math.abs((startDate - endDate) / oneDay));
+        // const range = moment().range(startDate,endDate)
+        if(startDate > endDate){
+            alert("Date is Not valid")
+        }
+        else{
+            setCutiData({...cutiData,rangeDate:diffDays})
+        }
+
+        return;
+
+
+    }
+
+
     onTrial = () => {
         this.props.navigation.navigate('Trial')
     }
     onTrial2 = () => {
         this.props.navigation.navigate('Trial2')
     }
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: "orange" }}>
+
+
+    const onChangeDateStart = async (e, ctx) => {
+        setCutiData({ ...cutiData, start: ctx })
+        setModaldate({ ...modalDate, start: false })
+    }
+
+    const onChangeDateEnd = async (e, ctx) => {
+
+        await setCutiData({ ...cutiData, end: ctx })
+        await setModaldate({ ...modalDate, end: false })
+        
+    }
+
+
+
+    return (
+        <View style={{ flex: 1, backgroundColor: "orange" }}>
+
             <KeyboardAvoidingView behavior="height">
+
+                {
+                    modalDate.start && (
+                        <DateTimePicker
+                            style={{ width: 0, height: 0 }}
+                            testID="dateTimePicker"
+                            value={new Date()}
+
+                            mode="date"
+                            is24Hour={true}
+                            dateFormat="dayofweek day month"
+                            display="default"
+                            onChange={(e, ctx) => { onChangeDateStart(e, ctx) }}
+                            onTouchEnd={() => setCutiData({ ...cutiData, start: new Date() })}
+                            onTouchEndCapture={() => setCutiData({ ...cutiData, start: new Date() })}
+
+                        // onTouchEnd={()=>setModaldate({...modalDate,start:false})}
+                        />
+
+                    )
+
+                }
+
+                {
+                    modalDate.end && (
+                        <DateTimePicker
+                            style={{ width: 0, height: 0 }}
+                            testID="dateTimePicker"
+                            value={new Date()}
+
+                            mode="date"
+                            is24Hour={true}
+                            dateFormat="dayofweek day month"
+                            display="default"
+                            onChange={(e, ctx) => { onChangeDateEnd(e, ctx) }}
+                            onTouchEnd={() => setCutiData({ ...cutiData, end: new Date() })}
+                            onTouchEndCapture={() => setCutiData({ ...cutiData, start: new Date() })}
+
+                        // onTouchEnd={()=>setModaldate({...modalDate,start:false})}
+                        />
+
+                    )
+
+                }
+
+
+
 
                 <View>
                     <View style={{ width: "100%", height: 150 }}>
                         <View style={{ paddingTop: "10%", alignItems: "center" }}>
-                            <Text style={{ fontSize: 35, color: "white" }}>CUTI</Text>
+                            <Text style={{ fontSize: 35, color: "white" }}>CUTI {cutiData.rangeDate}</Text>
                         </View>
                     </View>
                 </View>
@@ -42,38 +148,58 @@ class Cuti2 extends React.Component {
 
                             <View>
                                 <Text style={{ fontSize: 20, color: "orange" }}>Name</Text>
-                                <TextInput style={{  borderRadius: 15,borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
+                                <TextInput onChange={(text) => setCutiData({ ...cutiData, name: text })} style={{ borderRadius: 15, borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
                             </View>
                             <View>
                                 <Text style={{ fontSize: 20, color: "orange" }}>Position</Text>
-                                <TextInput style={{  borderRadius: 15,borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
+                                <TextInput
+                                    onChange={(text) => setCutiData({ ...cutiData, position: text })}
+                                    style={{ borderRadius: 15, borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
                             </View>
                             <View>
                                 <Text style={{ fontSize: 20, color: "orange" }}>Start - End</Text>
-                                <View style={{ justifyContent: "space-around", flexDirection: "row", width: "100%"}}>
-                                <TextInput style={{ width: "47%",borderRadius: 15,borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
-                                <Text style={{ fontSize: 20, color: "orange" }}>-</Text>
-                                <TextInput style={{ width: "47%", borderRadius: 15,borderWidth: 1, height: 36, borderColor: "gray", justifyContent: "center", backgroundColor: "#dddddd" }}></TextInput>
+                                <View style={{ justifyContent: "space-around", flexDirection: "row", width: "100%" }}>
+                                    <TouchableOpacity style={{ width: width * 0.45, height: 45, borderRadius: 25, backgroundColor: "#a55eea", }}
+                                        onPress={() => { setModaldate({ ...modalDate, start: true }) }}>
+                                        <View style={{ alignItems: "center", marginVertical: 11 }}>
+                                            <Text style={{ color: "white", fontSize: width * 0.04 }}>
+                                                <Text>
+                                                    {cutiData.start.toString()}
+                                                </Text>
+                                            </Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                    <Text style={{ fontSize: 20, color: "orange" }}>-</Text>
+                                    <TouchableOpacity style={{ width: width * 0.45, height: 45, borderRadius: 25, backgroundColor: "#a55eea", }}
+                                        onPress={() => { setModaldate({ ...modalDate, end: true }) }}>
+                                        <View style={{ alignItems: "center", marginVertical: 11 }}>
+                                            <Text style={{ color: "white", fontSize: width * 0.04 }}>
+                                                <Text>
+                                                    {cutiData.end.toString()}
+                                                </Text>
+                                            </Text>
+                                        </View>
+                                    </TouchableOpacity>
                                 </View>
                             </View>
                             <View>
                                 <Text style={{ fontSize: 20, color: "orange" }}>Neccessity</Text>
-                                <TextInput style={{ borderRadius: 15,borderWidth: 1, height: 150, borderColor: "gray", justifyContent: "flex-end", backgroundColor: "#dddddd" }}> </TextInput>
+                                <TextInput style={{ borderRadius: 15, borderWidth: 1, height: 150, borderColor: "gray", justifyContent: "flex-end", backgroundColor: "#dddddd" }}> </TextInput>
                             </View>
-                            
 
-                            <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 30}}>
-                                    <TouchableOpacity style={{ width: 120, height: 45, borderRadius: 25, backgroundColor: "#a55eea" }} onPress={() => { this.onTrial2() }}>
-                                        <View style={{ alignItems: "center", marginVertical: 11 }}>
-                                            <Text style={{ color: "white" }}>Clear</Text>
-                                        </View>
-                                    </TouchableOpacity>
 
-                                    <TouchableOpacity style={{ width: 120, height: 45, borderRadius: 25, backgroundColor: "#a55eea" }} onPress={() => { this.onTrial() }}>
-                                        <View style={{ alignItems: "center", marginVertical: 11 }}>
-                                            <Text style={{ color: "white" }}>Upload</Text>
-                                        </View>
-                                    </TouchableOpacity>
+                            <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 30 }}>
+                                <TouchableOpacity style={{ width: 120, height: 45, borderRadius: 25, backgroundColor: "#a55eea" }} onPress={() => { this.onTrial2() }}>
+                                    <View style={{ alignItems: "center", marginVertical: 11 }}>
+                                        <Text style={{ color: "white" }}>Clear</Text>
+                                    </View>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity style={{ width: 120, height: 45, borderRadius: 25, backgroundColor: "#a55eea" }} onPress={countRangeDate}>
+                                    <View style={{ alignItems: "center", marginVertical: 11 }}>
+                                        <Text style={{ color: "white" }}>Upload</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
                         </View>
                     </ScrollView>
@@ -81,8 +207,8 @@ class Cuti2 extends React.Component {
 
             </KeyboardAvoidingView>
         </View >
-        )
-    }
+    )
 }
+
 
 export default Cuti2;
